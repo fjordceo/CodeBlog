34744221ff72359e9f320a5e278e4cb112f054ad
Mikhail Lisaev <fjordceo@hotmail.com>
2025-06-26 11:03:12 +0700

    Добавьте файлы проекта.

---
+++ Episode001/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Episode001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
