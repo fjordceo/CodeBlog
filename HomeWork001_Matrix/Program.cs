5a1f8d6b0a02d1a200006d2469cfe4afa2140bc1
Mikhail Lisaev <fjordceo@hotmail.com>
2025-06-26 14:43:36 +0700

    Home Work 001 is completed

---
+++ HomeWork001_Matrix/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace HomeWork001_Matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("Wake up, Neo... ");
+            System.Threading.Thread.Sleep(3000); 
+            Console.Clear();
+
+            Console.Write("The Matrix has you... ");
+            System.Threading.Thread.Sleep(3000); 
+            Console.Clear();
+
+            Console.Write("Follow the white rabbit... ");
+            System.Threading.Thread.Sleep(3000); 
+            Console.Clear();
+
+            Console.Write("Knock, knock, Neo... ");
+            System.Threading.Thread.Sleep(3000); 
+            Console.Clear();
+            Console.ResetColor(); // Сброс цвета консоли
+            Console.Write("Press any key to exit...");
+
+        }
+    }
+}
+
+
+// Wake up, Neo... The Matrix has you... Follow the white rabbit... Knock, knock, Neo...
