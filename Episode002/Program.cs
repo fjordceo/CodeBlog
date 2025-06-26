9d2b96b60f53ed22dc4a3ad02c98b64fb2c55947
Mikhail Lisaev <fjordceo@hotmail.com>
2025-06-26 15:45:18 +0700

    Episode 001, 002, 003 is completed

---
+++ Episode002/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Episode002
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
--- HomeWork001_Matrix/Program.cs
+++ HomeWork001_Matrix/Program.cs
@@ -4,28 +4,33 @@
     {
         static void Main(string[] args)
         {
+            int delay = 3000; 
+
             Console.ForegroundColor = ConsoleColor.Green;
+
             Console.Write("Wake up, Neo... ");
-            System.Threading.Thread.Sleep(3000); 
-            Console.Clear();
+            ClearScreenWithDelay(delay);
 
             Console.Write("The Matrix has you... ");
-            System.Threading.Thread.Sleep(3000); 
-            Console.Clear();
+            ClearScreenWithDelay(delay);
 
             Console.Write("Follow the white rabbit... ");
-            System.Threading.Thread.Sleep(3000); 
-            Console.Clear();
+            ClearScreenWithDelay(delay);
 
             Console.Write("Knock, knock, Neo... ");
-            System.Threading.Thread.Sleep(3000); 
-            Console.Clear();
-            Console.ResetColor(); // Сброс цвета консоли
+            ClearScreenWithDelay(delay);
+
+            Console.ResetColor(); 
             Console.Write("Press any key to exit...");
 
+        }
+
+        static void ClearScreenWithDelay(int delay)
+        {
+            System.Threading.Thread.Sleep(delay);
+            Console.Clear();
         }
     }
 }
 
 
-// Wake up, Neo... The Matrix has you... Follow the white rabbit... Knock, knock, Neo...
