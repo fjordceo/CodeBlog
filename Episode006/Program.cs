7b766fcab61816109c504e0a1712c73f7b6df8d3
Mikhail Lisaev <fjordceo@hotmail.com>
2025-06-29 15:36:50 +0700

    Lesson 006 and Home Work 002 is completed

--- Episode006/Program.cs
+++ Episode006/Program.cs
@@ -2,12 +2,59 @@
 /*
  * Условные операторы. if, switch
  */
+
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            int money;
+            int age;
+
+            Console.Write("А ну-ка скажи сколько у тебя денег? ");
+            money = int.Parse(Console.ReadLine() ?? "0"); 
+
+            if (money >1000)
+            {
+                Console.WriteLine("Ну пошли в клуб!");
+            }
+            else
+            {
+                Console.WriteLine("Хрен тебе, а не клуб!");
+            }
+
+            // Логические операторы
+            // && - логическое И
+            // || - логическое ИЛИ
+            // ! - логическое НЕ
+
+            // Пример использования оператора Switch
+            switch (money)
+            {
+                case 0:
+                    Console.WriteLine("Ты вообще без денег!");
+                    break;
+                case 100:
+                    Console.WriteLine("Ну хоть что-то есть!");
+                    break;
+                case 1000:
+                    Console.WriteLine("Ну ты богатый, конечно!");
+                    break;
+                default:
+                    Console.WriteLine("У тебя денег больше чем 1000, но меньше чем 10000!");
+                    break;
+            }
             
+            // Оператор быстрой проверки (тернарный оператор)
+            Console.WriteLine("Сколько тебе лет?");
+            age = int.Parse(Console.ReadLine() ?? "0");
+            string massage = age > 18 ? "ну ты взрослый!" : "ты еще малой!";
+            Console.WriteLine(massage);
         }
     }
 }
+
+// Получить с консоли значение трех чисел с приведением к типу int. Условным оператором выбрать наибольшее из них.
+// Вывести это число на консоль.
+// Сравнить на четность с помощью Switch.
+// Проверить на < 100 с помощью тернарного оператора.
---
+++ HomeWork002_Int/Program.cs
@@ -0,0 +1,85 @@
+﻿namespace HomeWork002_Int;
+
+// Получить с консоли значение трех чисел с приведением к типу int.
+// Условным оператором выбрать наибольшее из них.
+// Вывести это число на консоль.
+// Сравнить на четность с помощью Switch.
+// Проверить на < 100 с помощью тернарного оператора.
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Основные переменные
+        int firstValue;
+        int secondValue;
+        int thirdValue;
+        
+        
+        // Запрос ввода значений с консоли
+        Console.Write("Введите первое число: ");
+        firstValue = int.Parse(Console.ReadLine() ?? "0");
+        Console.Write("Введите второе число: ");
+        secondValue = int.Parse(Console.ReadLine() ?? "0");
+        Console.Write("Введите третье число: ");
+        thirdValue = int.Parse(Console.ReadLine() ?? "0");
+        Console.WriteLine();
+        
+        // Определение наибольшего числа
+        if (firstValue >= secondValue && firstValue >= thirdValue)
+        {
+            Console.WriteLine($"Наибольшее число: {firstValue}");
+        }
+        else if (secondValue >= firstValue && secondValue >= thirdValue)
+        {
+            Console.WriteLine($"Наибольшее число: {secondValue}");
+        }
+        else
+        {
+            Console.WriteLine($"Наибольшее число: {thirdValue}");
+        }
+
+        Console.WriteLine();
+        
+        // Проверка на четность с помощью Switch
+        switch (firstValue % 2)
+        {
+            case 0:
+                Console.WriteLine($"{firstValue} - четное число");
+                break;
+            default:
+                Console.WriteLine($"{firstValue} - нечетное число");
+                break;
+        }
+        switch (secondValue % 2)
+        {
+            case 0:
+                Console.WriteLine($"{secondValue} - четное число");
+                break;
+            default:
+                Console.WriteLine($"{secondValue} - нечетное число");
+                break;
+        }
+        switch (thirdValue % 2)
+        {
+            case 0:
+                Console.WriteLine($"{thirdValue} - четное число");
+                break;
+            default:
+                Console.WriteLine($"{thirdValue} - нечетное число");
+                break;
+        }
+
+        Console.WriteLine();
+        
+        // Проверка на < 100 с помощью тернарного оператора
+        Console.WriteLine(firstValue < 100 ? $"{firstValue} меньше 100" : $"{firstValue} не меньше 100");
+        Console.WriteLine(secondValue < 100 ? $"{secondValue} меньше 100" : $"{secondValue} не меньше 100");
+        Console.WriteLine(thirdValue < 100 ? $"{thirdValue} меньше 100" : $"{thirdValue} не меньше 100");
+        
+        
+        Console.WriteLine();
+    }
+}
